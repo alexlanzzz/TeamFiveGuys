28e4bbdde690b963a2661b39e6dd578a32a3348e
hoation <wanghaotian2017@hotmail.com>
2024-09-21 16:01:37 +1000

    helm_animation

--- src/js/test.js
+++ src/js/test.js
@@ -8,3 +8,32 @@ menuIcon.addEventListener("click", function () {
         sideMenu.classList.add("open");
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const helm = document.querySelector('.helm');
+    const locations = document.querySelectorAll('.location');
+    let currentRotation = 0;
+
+    document.querySelector('.helm-container').addEventListener('mousemove', function (event) {
+        const helmContainerRect = this.getBoundingClientRect();
+        const containerCenterX = helmContainerRect.left + helmContainerRect.width / 2;
+        const mouseX = event.clientX;
+
+        const rotationSpeed = 0.2;  // Rotation speed can be adjusted
+
+        if (mouseX < containerCenterX) {
+            currentRotation -= rotationSpeed;
+        } else {
+            currentRotation += rotationSpeed;
+        }
+
+        // Rotate the helm
+        helm.style.transform = `rotate(${currentRotation}deg)`;
+
+        // Rotate the locations along with the helm
+        locations.forEach(location => {
+            const angle = parseInt(location.getAttribute('data-angle'));
+            location.style.transform = `rotate(${angle - currentRotation}deg) translate(300px)`;
+        });
+    });
+});
